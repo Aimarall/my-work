86713a70437c3f7ad1c9f580c6d2413b86f90a30
Aimarall <aimaral21072004@gmail.com>
2021-05-06 14:57:42 +0600

    wdefrghn

--- src/redux/reducer/todo.js
+++ src/redux/reducer/todo.js
@@ -3,7 +3,23 @@ const initialState = {};
 const todo = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_TODO":
-            const newState = { ...state };
-            const id = Math.random() 
+            const addState = { ...state };
+            const id = Math.random() * 9999999999;
+            addState[id.toFixed(0)] = {
+                text: action.text
+            };
+            return addState;
+        case "REMOVE_TODO":
+            const removeState = { ...state };
+            delete removeState[action.id];
+            return removeState;
+
+
+
+
+        default:
+            return state;
     }
 }
+
+export default todo;
