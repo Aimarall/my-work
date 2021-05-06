39ce3caae2ef02a80ed2f0ca1007cb93b7af86d5
Aimarall <aimaral21072004@gmail.com>
2021-05-06 15:44:05 +0600

    added fnctions to htis

---
+++ src/redux/actions/todo.js
@@ -0,0 +1,25 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./types";
+
+export const add = (text) => ({
+  type: ADD_TODO,
+  text: text,
+});
+
+export const remove = (id) => ({
+  type: REMOVE_TODO,
+  id: id,
+});
+
+export const toggle = (id) => ({
+  type: TOGGLE_TODO,
+  id: id,
+});
+
+export const delayedAdd = (text) => {
+  return (dispatch) => {
+    setTimeout(
+      () => dispatch(add(text)),
+      2000
+    );
+  };
+}
