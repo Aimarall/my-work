9d0c9a295e124074c0eadd659090681b88ebf3bb
Aimarall <aimaral21072004@gmail.com>
2021-05-06 14:57:13 +0600

    changed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
   const items = useSelector(state => state);
 
-  function onAddCalback(event) {
-    const data = new FormData(event.target)
-    
+  function onAddCallback(event) {
+    const data = new FormData(event.target);
+    dispatch({ type: "ADD_TODO", text: data.get('text') });
+
+    event.preventDefault();
   }
+
+  function onRemoveCallback(id) {
+    dispatch({ type: "REMOVE_TODO", id: id });
+  }
+
+  const output = Object.keys(items).map(id => (
+    <li key={id}>
+      <span>{items[id].text}</span>
+      <button onClick={() => onRemoveCallback(id)}>Remove</button>
+    </li>
+  ));
 
   return (
     <div className="App">
       <form onSubmit={onAddCallback}>
-        <input type="text" name="text" required/>
+        <input type="text" name="text" required />
         <button>Add</button>
       </form>
       <ul>
-
+        {output}
       </ul>
     </div>
   );
 }
 
 export default App;
-
-
-
