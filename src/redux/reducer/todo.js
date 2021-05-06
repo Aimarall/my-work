f5e5dd0799cb9da4b9ab0e9039fe954983d28fc5
Aimarall <aimaral21072004@gmail.com>
2021-05-06 15:44:30 +0600

    chaged of  this files

--- src/redux/reducer/todo.js
+++ src/redux/reducer/todo.js
@@ -1,23 +1,34 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "../actions/types";
+
 const initialState = {};
+
+const add = (state, action) => {
+    const id = Math.random() * 9999999999;
+    state[id.toFixed(0)] = {
+        text: action.text,
+        done: false
+    };
+    return state;
+}
+
+const remove = (state, action) => {
+    delete state[action.id];
+    return state;
+}
+
+const toggle = (state, action) => {
+    state[action.id].done = !state[action.id].done;
+    return state;
+}
 
 const todo = (state = initialState, action) => {
     switch (action.type) {
-        case "ADD_TODO":
-            const addState = { ...state };
-            const id = Math.random() * 9999999999;
-            addState[id.toFixed(0)] = {
-                text: action.text,
-                done: false
-            };
-            return addState;
-        case "REMOVE_TODO":
-            const removeState = { ...state };
-            delete removeState[action.id];
-            return removeState;
-        case "TOGGLE_TODO":
-            const toggleState = { ...state };
-            toggleState[action.id].done = !toggleState[action.id].done;
-            return toggleState;
+        case ADD_TODO:
+            return add({ ...state }, action);
+        case REMOVE_TODO:
+            return remove({ ...state }, action);
+        case TOGGLE_TODO:
+            return toggle({ ...state }, action);
 
         default:
             return state;
